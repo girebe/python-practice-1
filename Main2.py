8c94ddbdc9f5743353a40544dfabbdde03d827a4
girebe <sakharinamir@gmail.com>
2026-03-27 08:30:18 +0500

    trfyui

--- Main2.py
+++ Main2.py
@@ -26,10 +26,38 @@ while True:
         items += 1
         subtotal += price
     else:break
+    
+if subtotal < 3000:
+    discount = 0
+    discount_tier = 'No discount'
+    
+elif 3000 <= subtotal <7000:
+    discount = int(subtotal*0.05)
+    subtotal *= int(0.95)
+    discount_tier = 5
+    
+else:
+    discount = int(subtotal*0.15)
+    subtotal = int(subtotal*0.85)
+    discount_tier = 15
 
 print(f'Customer: {name}')
 print(f'Items: {items}')
 print(f'Subtotal: {subtotal}')
+print('-'*20)
+print(f'Discount tier: {discount_tier}')
+print(f'Discount: {discount} KZT')
+print(f'Total: {subtotal}')
+
+print('-'*20)
+print(f'Name uppercase: {name.upper()}\nName lowercase: {name.lower()}\nName length: {len(name)}')
+if len(name) >5:
+    print('long name')
+else:
+    print('short name')
+
+
+
 
 
 
